add49777ab488a7a4edca10c55df5ea4460b3a4c
Alexandr Metreniuc <metreniuk@gmail.com>
2023-12-05 09:25:26 +0200

    day 4 part 2 incomplete

--- src/day_four/mod.rs
+++ src/day_four/mod.rs
@@ -1,7 +1,7 @@
 use std::collections::HashMap;
 
 pub fn process_day() {
-    let file = std::fs::read_to_string("inputs/day-4-large.txt").unwrap();
+    let file = std::fs::read_to_string("inputs/day-4-small.txt").unwrap();
 
     let res: Vec<_> = file
         .lines()
@@ -29,17 +29,22 @@ pub fn process_day() {
 
     res.iter().enumerate().for_each(|(idx, x)| {
         let end = idx + x;
+        let times = *total_cards.get(&idx).unwrap_or(&1);
+        // println!("{idx} {x} {times}");
 
-        for _ in 0..*total_cards.get(&idx).unwrap_or(&1) {
+        for _ in 0..times {
             for n in idx..=end {
-                let entry = total_cards.entry(n).or_insert(0);
+                println!("{n} {idx} {x}");
 
-                *entry += 1;
+                total_cards.entry(n).and_modify(|entry| {
+                    *entry += 1;
+                });
             }
         }
+        // println!("{:?}", total_cards);
     });
 
     let cards_count: usize = total_cards.values().sum();
 
-    println!("res {:?}", cards_count);
+    println!("res {:?} {}", res, cards_count);
 }
