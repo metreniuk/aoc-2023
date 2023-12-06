b2f5d1a7262dde0609ad6a51708e05a9efc0a64f
Alexandr Metreniuc <metreniuk@gmail.com>
2023-12-06 09:56:39 +0200

    day 6 part 2

--- src/day_six/mod.rs
+++ src/day_six/mod.rs
@@ -1,38 +1,34 @@
 pub fn process_day() {
-    let file = std::fs::read_to_string("inputs/day-6-small.txt").unwrap();
-    let mut times: Vec<_> = Vec::new();
-    let mut distances: Vec<_> = Vec::new();
+    let file = std::fs::read_to_string("inputs/day-6-large.txt").unwrap();
+    let mut time: isize = 0;
+    let mut distance: isize = 0;
 
     for line in file.lines() {
         if line.starts_with("Time:") {
-            times = line
+            time = line
                 .split_whitespace()
                 .skip(1)
-                .map(|x| x.parse::<isize>().unwrap())
-                .collect();
+                .fold(String::new(), |s, x| s + x)
+                .parse::<isize>()
+                .unwrap();
         }
         if line.starts_with("Distance:") {
-            distances = line
+            distance = line
                 .split_whitespace()
                 .skip(1)
-                .map(|x| x.parse::<isize>().unwrap())
-                .collect();
+                .fold(String::new(), |s, x| s + x)
+                .parse::<isize>()
+                .unwrap();
         }
     }
 
-    let data: usize = times
-        .iter()
-        .zip(distances.iter())
-        .map(|(time, distance)| {
-            (1..*time)
-                .filter(|speed| {
-                    let remaining_time = time - speed;
-                    let traveled = remaining_time * speed;
-                    traveled > *distance
-                })
-                .count()
+    let data: usize = (1..time)
+        .filter(|speed| {
+            let remaining_time = time - speed;
+            let traveled = remaining_time * speed;
+            traveled > distance
         })
-        .product();
+        .count();
 
-    println!("{:?}", data);
+    println!("{}", data);
 }
